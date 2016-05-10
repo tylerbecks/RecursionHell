1590e3828f3bcccd99ea8f84b4f3bcbda1d2edd3
Dennis Ting <dennisting@gmail.com>
2016-05-10 04:22:41 -0700

    Add function implementations to make tests pass

--- src/recursion-hell.js
+++ src/recursion-hell.js
@@ -7,6 +7,10 @@
  *   factorial(5); // 120
  */
 function factorial(n) {
+  if (n < 0) return undefined;
+  if (n === 0) return 1;
+  if (n < 3) return n;
+  return n * factorial(n - 1);
 }
 
 /**
@@ -18,6 +22,8 @@ function factorial(n) {
  * @returns {boolean}
  */
 function palindrome(str) {
+  if (str.length < 2) return true;
+  return str[0] === str[str.length - 1] && palindrome(str.substring(1, -1));
 }
 
 /**
@@ -28,6 +34,8 @@ function palindrome(str) {
  *   stringReverse('testing'); // 'gnitset'
  */
 function stringReverse(str) {
+  if (str.length === 0) return '';
+  return stringReverse(str.substring(1)) + str[0];
 }
 
 /**
@@ -42,6 +50,8 @@ function stringReverse(str) {
  *   // ['A', 'B', 'C']
  */
 function map(arr, fn) {
+  if (arr.length === 0) return [];
+  return [fn(arr[0])].concat(map(arr.slice(1), fn));
 }
 
 /**
@@ -50,6 +60,7 @@ function map(arr, fn) {
  * Returns the greatest common divisor (gcd) of two positive numbers.
  */
 function gcd(x, y) {
+  return !y ? x : gcd(y, x % y);
 }
 
 /**
@@ -60,6 +71,7 @@ function gcd(x, y) {
  *   inBetween(4, 8); // [5, 6, 7]
  */
 function inBetween(x, y) {
+  return x + 1 < y ? [x + 1].concat(inBetween(x + 1, y)) : [];
 }
 
 /**
@@ -71,6 +83,7 @@ function inBetween(x, y) {
  *   arraySum([5, 2, 6]); // 13
  */
 function arraySum(arr) {
+  return arr.length ? arr[0] + arraySum(arr.slice(1)) : 0;
 }
 
 /**
@@ -82,6 +95,7 @@ function arraySum(arr) {
  *   exp(8, 2); // 64
  */
 function exp(base, pow) {
+  return pow === 0 ? 1 : base * exp(base, pow - 1);
 }
 
 /**
@@ -90,6 +104,9 @@ function exp(base, pow) {
  * Returns the nth number of the fibonacci sequence.
  */
 function fib(n) {
+  if (n < 0) return undefined;
+  if (n < 2) return n;
+  return fib(n - 1) + fib(n - 2);
 }
 
 /**
@@ -100,6 +117,24 @@ function fib(n) {
  * @returns {boolean}
  */
 function even(n) {
+  if (n < 2 && n > -2) return n === 0;
+  return n > 0 ? even(n - 2) : even(n + 2);
+}
+
+function _merge(arr1, arr2) {
+  var i = 0;
+  var j = 0;
+  var result = [];
+  while (i < arr1.length && j < arr2.length) {
+    arr1[i] < arr2[j] ? result.push(arr1[i++]) : result.push(arr2[j++]);
+  }
+  while (i < arr1.length) {
+    result.push(arr1[i++]);
+  }
+  while (j < arr2.length) {
+    result.push(arr2[j++]);
+  }
+  return result;
 }
 
 /**
@@ -108,7 +143,28 @@ function even(n) {
  * Returns an array sorted using merge sort.
  */
 function mergeSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+  if (arr.length === 2) {
+    return arr[0] > arr[1] ? [arr[1], arr[0]] : [arr[0], arr[1]];
+  }
+  var mid = Math.floor(arr.length / 2);
+  return _merge(mergeSort(arr.slice(0, mid)), mergeSort(arr.slice(mid)));
 }
+
+var _numToTextMap = {
+  0: 'zero',
+  1: 'one',
+  2: 'two',
+  3: 'three',
+  4: 'four',
+  5: 'five',
+  6: 'six',
+  7: 'seven',
+  8: 'eight',
+  9: 'nine'
+};
 
 /**
  * NUMBERS TO TEXT
@@ -119,6 +175,14 @@ function mergeSort(arr) {
  *   numToText('I have 5 dollars); // 'I have five dollars'
  */
 function numToText(s, idx) {
+  idx = idx || 0;
+  if (idx >= s.length) return '';
+  if (/[0-9]/.test(s[idx]) &&
+      ((!s[idx - 1] || s[idx - 1] === ' ') &&
+      (!s[idx + 1] || s[idx + 1] === ' '))) {
+    return _numToTextMap[+s[idx]] + numToText(s, idx + 1);
+  }
+  return s[idx] + numToText(s, idx + 1);
 }
 
 /**
@@ -128,6 +192,9 @@ function numToText(s, idx) {
  * addition and subtraction math operators.
  */
 function mul(x, y) {
+  if (y < 0) return mul(-x, -y);
+  if (x === 0 || y === 0) return 0;
+  return y === 1 ? x : x + mul(x, y - 1);
 }
 
 /**
@@ -137,6 +204,19 @@ function mul(x, y) {
  * addition and subtraction math operators.
  */
 function divide(x, y) {
+  if (y === 0) return undefined;
+  if (x < 0) {
+    return divide(-x, -y);
+  }
+  if (y < 0) {
+    if (x + y < 0) return 0;
+    if (x + y === 0) return -1;
+    return -1 + divide(x + y, y);
+  } else {
+    if (x - y < 0) return 0;
+    if (x - y === 0) return 1;
+    return 1 + divide(x - y, y);
+  }
 }
 
 /**
@@ -149,6 +229,9 @@ function divide(x, y) {
  *   powerOfTwo(10); // false
  */
 function powerOfTwo(n) {
+  if (n < 1) return false;
+  if (n === 1) return true;
+  return powerOfTwo(n / 2);
 }
 
 /**
@@ -162,6 +245,24 @@ function powerOfTwo(n) {
  *   modulo(22,6); // 4
  */
 function modulo(x, y) {
+  if (y === 0) return undefined;
+  if (x < 0) {
+    if (y < 0) {
+      if (x - y > 0) return x;
+      return modulo(x - y, y);
+    } else {
+      if (x + y > 0) return x;
+      return modulo(x + y, y);
+    }
+  } else {
+    if (y < 0) {
+      if (x + y < 0) return x;
+      return modulo(x + y, y);
+    } else {
+      if (x - y < 0) return x;
+      return modulo(x - y, y)
+    }
+  }
 }
 
 /**
@@ -174,6 +275,9 @@ function modulo(x, y) {
  *   compareStr('tomato', 'tomato'); // true
  */
 function compareStr(str1, str2) {
+  var idx = arguments[2] || 0;
+  if (idx >= str1.length && idx > str2.length) return true;
+  return str1[idx] === str2[idx] && compareStr(str1, str2, idx + 1);
 }
 
 /**
@@ -182,6 +286,7 @@ function compareStr(str1, str2) {
  * Returns an array with the elements of array, arr, in reverse order.
  */
 function reverseArr(arr) {
+  return arr.length === 0 ? [] : reverseArr(arr.slice(1)).concat([arr[0]]);
 }
 
 /**
@@ -193,6 +298,7 @@ function reverseArr(arr) {
  *   buildList(7, 3); // [7, 7, 7]
  */
 function buildList(val, len) {
+  return len <= 0 ? [] : [val].concat(buildList(val, len - 1));
 }
 
 /**
@@ -204,6 +310,12 @@ function buildList(val, len) {
  *   countOccurrence([2,'banana',4,4,1,'banana'], 'banana'); // 2
  */
 function countOccurrence(arr, val) {
+  if (arr.length <= 0) return 0;
+  return +(arr[0] === val) + countOccurrence(arr.slice(1), val);
+}
+
+function _isObject(obj) {
+  return obj === Object(obj);
 }
 
 /**
@@ -216,6 +328,11 @@ function countOccurrence(arr, val) {
  *   countKeysInObj(obj, 'e'); // 2
  */
 function countKeysInObj(obj, key) {
+  if (!_isObject(obj)) return 0;
+  return Object.keys(obj).reduce(function(count, k) {
+    console.log(obj, k);
+    return count + +(k === key) + countKeysInObj(obj[k], key);
+  }, 0);
 }
 
 /**
@@ -228,6 +345,12 @@ function countKeysInObj(obj, key) {
  *   countValuesInObj(obj, 'e') // 1
  */
 function countValuesInObj(obj, val) {
+  if (!_isObject(obj)) {
+    return +(obj === val);
+  }
+  return Object.keys(obj).reduce(function(count, k) {
+    return count + countValuesInObj(obj[k], val);
+  }, 0);
 }
 
 /**
@@ -239,6 +362,16 @@ function countValuesInObj(obj, val) {
  * @returns {Object} object with renamed keys
  */
 function replaceKeysInObj(obj, key, newKey) {
+  if (_isObject(obj)) {
+    if (obj.hasOwnProperty(key)) {
+      obj[newKey] = obj[key];
+      delete obj[key];
+    }
+    Object.keys(obj).forEach(function(k) {
+      replaceKeysInObj(obj[k], key, newKey);
+    });
+  }
+  return obj;
 }
 
 /**
@@ -250,6 +383,8 @@ function replaceKeysInObj(obj, key, newKey) {
  *   capitalizeWords(words); // ['I', 'AM', 'LEARNING', 'RECURSION']
  */
 function capitalizeWords(arr) {
+  if (arr.length === 0) return [];
+  return [arr[0].toUpperCase()].concat(capitalizeWords(arr.slice(1)));
 }
 
 /**
@@ -262,6 +397,9 @@ function capitalizeWords(arr) {
  *   capitalizeWords(words); // ['Car', 'Poop', 'Banana']
  */
 function capitalizeFirst(arr) {
+  if (arr.length === 0) return [];
+  return [arr[0][0].toUpperCase() + arr[0].substr(1)]
+    .concat(capitalizeFirst(arr.slice(1)));
 }
 
 /**
@@ -280,6 +418,12 @@ function capitalizeFirst(arr) {
  *   nestedEvenSum(obj); // 10
  */
 function nestedEvenSum(obj) {
+  if (!_isObject(obj)) {
+    return obj % 2 === 0 ? obj : 0;
+  }
+  return Object.keys(obj).reduce(function(count, k) {
+    return count + nestedEvenSum(obj[k]);
+  }, 0);
 }
 
 /**
@@ -290,6 +434,9 @@ function nestedEvenSum(obj) {
  *   flatten([1,[2],[3,[[4]]],5]); // [1,2,3,4,5]
  */
 function flatten(arr) {
+  if (arr.length === 0) return [];
+  var cur = Array.isArray(arr[0]) ? flatten(arr[0]) : [arr[0]];
+  return cur.concat(flatten(arr.slice(1)));
 }
 
 /**
@@ -300,6 +447,10 @@ function flatten(arr) {
  *   letterTally('potato'); // {'p':1, 'o':2, 't':2, 'a':1}
  */
 function letterTally(str, obj) {
+  obj = obj || {};
+  if (!str.length) return obj;
+  obj[str[0]] = (obj[str[0]] || 0) + 1;
+  return letterTally(str.substr(1), obj);
 }
 
 /**
@@ -313,6 +464,10 @@ function letterTally(str, obj) {
  *   compress([1, 2, 2, 3, 4, 4, 2, 5, 5, 5, 4, 4]); // [1, 2, 3, 4, 2, 5, 4]
  */
 function compress(arr) {
+  if (!arr.length) return [];
+  var prev = arguments[1];
+  return (prev === arr[0] ? [] : [arr[0]])
+    .concat(compress(arr.slice(1), arr[0]));
 }
 
 /**
@@ -324,6 +479,18 @@ function compress(arr) {
  *   minimizeZeroes([2, 0, 0, 0, 1, 0, 0, 4]); // [2, 0, 1, 0, 4]
  */
 function minimizeZeroes(arr) {
+  if (!arr.length) return [];
+  var prev = arguments[1];
+  return (prev === 0 && prev === arr[0] ? [] : [arr[0]])
+    .concat(compress(arr.slice(1), arr[0]));
+}
+
+function _makeNegative(n) {
+  return n < 0 ? n : -n;
+}
+
+function _makePositive(n) {
+  return n < 0 ? -n : n;
 }
 
 /**
@@ -337,6 +504,10 @@ function minimizeZeroes(arr) {
  *   alternateSign([-2, -7, 8, 3, -1, 4]); // [2, -7, 8, -3, 1, -4]
  */
 function alternateSign(arr) {
+  if (!arr.length) return [];
+  var prev = arguments[1] || -1;
+  return (prev > 0 ? [_makeNegative(arr[0])] : [_makePositive(arr[0])])
+    .concat(alternateSign(arr.slice(1), -prev));
 }
 
 // http://kevvv.in/untitledrecursion-in-javascript/
